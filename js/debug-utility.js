66985891655aef13b5147dd9956d3310608f0efc
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2016-01-27 21:21:00 +0900

    Update style

--- js/debug-utility.js
+++ js/debug-utility.js
@@ -19,16 +19,16 @@
    'profileEnd'
   ];
 
-  if(typeof window.console === "undefined"){
+  if(typeof window.console === 'undefined'){
     window.console = {};
   }
 
   for(var i in methods){
     (function( m ){
-      if (console[m] && debugMode && typeof console[m] === "function") {
+      if (console[m] && debugMode && typeof console[m] === 'function') {
         window[m] =
           function() {
-            console[m].apply( console, arguments );
+            console[m].apply(console, arguments);
           };
       }
       else{
